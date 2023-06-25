3c562aeef7b1ab4468b9475c4e803d922dc35c57
ShelissDev <sheliss.kai@gmail.com>
2023-06-25 19:27:49 +0300

    profile update wip

--- frontend/src/app/components/profile-update/profile-update.component.ts
+++ frontend/src/app/components/profile-update/profile-update.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { ProfileData } from 'src/app/Interfaces';
+import { AccountService } from 'src/app/services/account.service';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-profile-update',
@@ -13,6 +17,8 @@ import { MatButtonModule } from '@angular/material/button';
   imports: [
     CommonModule,
     RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
     MatCardModule, 
     MatFormFieldModule, 
     MatInputModule,
@@ -21,6 +27,35 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './profile-update.component.html',
   styleUrls: ['./profile-update.component.scss']
 })
-export class ProfileUpdateComponent {
+export class ProfileUpdateComponent implements OnInit {
+  error: string = '';
+  profile: ProfileData = {
+    name: '',
+    email: ''
+  }
 
+  constructor (
+    private accountService: AccountService
+  ) {}
+
+  ngOnInit(): void {
+    this.accountService.getProfile().subscribe({
+      next: (res) => {
+        this.profile = res;
+      },
+      error: (err) => {
+        this.error = err.toString();
+      }
+    })
+  }
+
+
+
+  updateForm = new FormGroup({
+    changeEmail: new FormControl(this.profile.email, [Validators.email]),
+    changeName: new FormControl(''),
+    changeNewPassword: new FormControl(''),
+    changeOldPassword: new FormControl('', [Validators.required])
+
+  })
 }
