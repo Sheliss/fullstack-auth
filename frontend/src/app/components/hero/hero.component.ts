f8a74f42ba6663d03d60003ec3b1437db08d09c7
ShelissDev <sheliss.kai@gmail.com>
2023-06-27 12:03:05 +0300

    hero different buttons for logged in user

--- frontend/src/app/components/hero/hero.component.ts
+++ frontend/src/app/components/hero/hero.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { currentUser } from 'src/app/Interfaces';
+import { UserService } from 'src/app/services/user.service';
+import { AccountService } from 'src/app/services/account.service';
+
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatButtonModule } from '@angular/material/button';
@@ -9,8 +14,30 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.scss'],
   standalone: true,
-  imports: [ RouterModule, MatCardModule, MatGridListModule, MatButtonModule ]
+  imports: [ CommonModule, RouterModule, MatCardModule, MatGridListModule, MatButtonModule ]
 })
-export class HeroComponent {
+export class HeroComponent implements OnInit {
+  currentUser: currentUser = {
+    isLogged: false,
+    name: ''
+  }
+
+  constructor(
+    private userService: UserService,
+    private accountService: AccountService
+  ) {}
+
+  ngOnInit(): void {
+    this.userService.getUser();
+    this.userService.currentUser.subscribe((user: currentUser) => this.currentUser = user);
+  }
+
+  onLogout() {
+    this.accountService.onLogout().subscribe(
+      () => {
+        this.userService.setUser(false, '');
+      }
+    );
+  }
 
 }
